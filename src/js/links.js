d3bab7a63aa63884ee1a49fcae4a01dc01e7c14a
Shane <6071159+smashedr@users.noreply.github.com>
2023-10-19 22:19:46 -0700

    Update Table Function

--- src/js/links.js
+++ src/js/links.js
@@ -11,20 +11,16 @@ const onlyDomains = urlParams.get('onlyDomains')
 console.log(`onlyDomains: ${onlyDomains}`)
 const filteringDomains = urlParams.get('filteringDomains')
 console.log(`filteringDomains: ${filteringDomains}`)
+
+// Prompt for pattern if filtering is enabled
 const pattern = filtering === 'true' ? window.prompt('Enter Filter:') : null
 console.log(`pattern: ${pattern}`)
-
-const messageEl = document.getElementById('message')
-const linksEl = document.getElementById('links')
-const domainsEl = document.getElementById('domains')
 
 chrome.tabs.sendMessage(tabId, { action: 'extract' }, (links) => {
     console.log('sendMessage')
     console.log(links)
     processLinks(links, pattern, onlyDomains)
 })
-
-// TODO: Cleanup Below Functions
 
 /**
  * Process Links
@@ -41,86 +37,64 @@ function processLinks(links, pattern, onlyDomains) {
         return window.alert(chrome.runtime.lastError)
     }
 
-    // LINKS
     // Filter bad links like: javascript:void(0)
     const filteredLinks = links.filter(
         (link) => link.lastIndexOf('://', 10) > 0
     )
+
     // Remove duplicate and sort links
     let items = [...new Set(filteredLinks)].sort()
-    // Filter links based on pattern.
+
+    // Filter links based on pattern
     const re = pattern ? new RegExp(pattern, 'g') : null
     if (re) {
         console.log(`Filtering Links by: ${re}`)
         items = items.filter((item) => item.match(re))
     }
-    console.log('items')
-    console.log(items)
+
+    const messageEl = document.getElementById('message')
     if (!items.length) {
         messageEl.textContent = 'No matches'
         console.log('return')
         return
     }
 
-    let linksTable = document
-        .getElementById('links')
-        .getElementsByTagName('tbody')[0]
-    items.forEach(function (value, i) {
-        addUrl(linksTable, value)
-    })
+    if (onlyDomains !== 'true') {
+        console.log('updating links now...')
+        updateTable(items, 'links')
+    } else {
+        document.getElementById('links-heading').style.display = 'None'
+    }
 
     // Extract domains from items and sort
     const domains = [...new Set(items.map((link) => getBaseURL(link)))].sort()
+    if (!domains.length) {
+        document.getElementById('links-heading').style.display = 'None'
+    }
+    updateTable(domains, 'domains')
 
-    let domainsTable = document
-        .getElementById('domains')
-        .getElementsByTagName('tbody')[0]
-    domains.forEach(function (value, i) {
-        addUrl(domainsTable, value)
-    })
-    // console.log('domains')
-    // console.log(domains)
-    // const reDomains = filteringDomains ? re : null
-    // domains.forEach(
-    //     (domain) => addNodes(domain, domainsEl, reDomains),
-    //     onlyDomains
-    // )
     messageEl.style.display = 'None'
 }
 
-function addUrl(element, url) {
-    // let copyLink = document.createTextNode(i + 1)
-    // let copyLink = document.createElement('a')
-    // copyLink.text = 1 + i
-    // copyLink.title = name
-    // copyLink.href = value
-    // copyLink.target = '_blank'
-
-    let link = document.createElement('a')
-    link.text = url
-    link.href = url
-    link.target = '_blank'
-
-    let row = element.insertRow()
-    let cell1 = row.insertCell()
-    cell1.appendChild(link)
-    // let cell2 = row.insertCell()
-    // cell2.appendChild(fileLink)
-}
-
 /**
- * Add nodes to container
+ * Update Table with URLs
  * @function addNodes
- * @param url
- * @param {Node} container
+ * @param {Array} data
+ * @param {String} elementId
  */
-function addNodes(url, container) {
-    const br = document.createElement('br')
-    const a = document.createElement('a')
-    a.href = url
-    a.innerText = url
-    container.appendChild(a)
-    container.appendChild(br)
+function updateTable(data, elementId) {
+    let tbody = document
+        .getElementById(elementId)
+        .getElementsByTagName('tbody')[0]
+    data.forEach(function (url, i) {
+        let link = document.createElement('a')
+        link.text = url
+        link.href = url
+        link.target = '_blank'
+        let row = tbody.insertRow()
+        let cell1 = row.insertCell()
+        cell1.appendChild(link)
+    })
 }
 
 /**
