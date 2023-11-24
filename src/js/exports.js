15720a3d30375d7fcfda98ac1d1376678992906c
Shane <6071159+smashedr@users.noreply.github.com>
2023-11-23 23:37:56 -0800

    Update Context Menu Creation

--- src/js/exports.js
+++ src/js/exports.js
@@ -8,32 +8,23 @@
 export function createContextMenus(patterns) {
     const ctx = ['page', 'link', 'selection']
     const contexts = [
-        [['link'], 'copy', 'Copy Link Text to Clipboard'],
-        [['selection'], 'selection', 'Extract from Selection'],
-        [['selection', 'link'], 'separator', 'seperator-top'],
-        [ctx, 'filters', 'Extract with Filter'],
-        [ctx, 'links', 'Extract All Links'],
-        [ctx, 'domains', 'Extract All Domains'],
-        [ctx, 'separator', 'separator-bottom'],
-        [ctx, 'options', 'Open Options'],
+        [['link'], 'copy', 'normal', 'Copy Link Text to Clipboard'],
+        [['selection'], 'selection', 'normal', 'Extract from Selection'],
+        [['selection', 'link'], 'separator-1', 'separator', 'separator'],
+        [ctx, 'filters', 'normal', 'Extract with Filter'],
+        [ctx, 'links', 'normal', 'Extract All Links'],
+        [ctx, 'domains', 'normal', 'Extract All Domains'],
+        [ctx, 'separator-2', 'separator', 'separator'],
+        [ctx, 'options', 'normal', 'Open Options'],
     ]
-    for (const context of contexts) {
-        if (context[1] === 'separator') {
-            chrome.contextMenus.create({
-                type: context[1],
-                contexts: context[0],
-                id: context[2],
-            })
-        } else {
-            chrome.contextMenus.create({
-                title: context[2],
-                contexts: context[0],
-                id: context[1],
-            })
-        }
-    }
-    // const { patterns } = await chrome.storage.sync.get(['patterns'])
-    // console.log('patterns', patterns)
+    contexts.forEach((context) => {
+        chrome.contextMenus.create({
+            contexts: context[0],
+            id: context[1],
+            type: context[2],
+            title: context[3],
+        })
+    })
     if (patterns) {
         patterns.forEach((pattern, i) => {
             console.log(`pattern: ${i}: ${pattern}`)
