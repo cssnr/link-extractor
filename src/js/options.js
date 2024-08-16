40e894e851cebfc54e3ff6a315cd95f21e2cbfc4
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2024-08-15 19:40:44 -0700

    Fix Filter Delete (#96)
    * Fix Filter Delete Button
    
    * Cleanup Logging

--- src/js/options.js
+++ src/js/options.js
@@ -154,22 +154,22 @@ async function addFilter(event) {
  * Delete Filter
  * @function deleteFilter
  * @param {MouseEvent} event
- * @param {String} index
+ * @param {String} [index]
  */
 async function deleteFilter(event, index = undefined) {
-    console.debug('deleteFilter:', event)
+    console.debug('deleteFilter:', index, event)
     event.preventDefault()
+    const filter = event.currentTarget?.dataset?.value
+    console.debug('filter:', filter)
     const { patterns } = await chrome.storage.sync.get(['patterns'])
     // console.debug('patterns:', patterns)
     if (!index) {
         // const anchor = event.target.closest('a')
-        const filter = event.currentTarget?.dataset?.value
-        console.log(`filter: ${filter}`)
         if (filter && patterns.includes(filter)) {
             index = patterns.indexOf(filter)
         }
     }
-    console.debug(`index: ${index}`)
+    console.debug('index:', index)
     if (index !== undefined) {
         const name = patterns[index]
         patterns.splice(index, 1)
