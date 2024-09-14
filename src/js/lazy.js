07d40422d3181725dd7603c947418706cd9c80cd
Shane <6071159+smashedr@users.noreply.github.com>
2024-09-13 17:00:22 -0700

    Update Lazy Loading Background Color

--- src/js/lazy.js
+++ src/js/lazy.js
@@ -10,6 +10,19 @@ link.rel = 'icon'
 link.href = `${url.origin}/favicon.ico`
 document.head.appendChild(link)
 
+let theme = localStorage.getItem('theme')
+if (!theme || theme === 'auto') {
+    theme = window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light'
+}
+console.debug('theme:', theme)
+if (theme === 'dark') {
+    document.body.style.backgroundColor = '#1c1b21'
+} else {
+    document.body.style.backgroundColor = '#fff'
+}
+
 window.addEventListener('focus', () => {
     console.log('url:', url)
     window.location = url.href
